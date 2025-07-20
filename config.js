f9b9e33a09b8064f91c79101001cbcc46c711e09
baone9 <luonghoaibao220109@gmail.com>
2025-07-20 22:22:27 +0700

    Update config.jsq

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'lenn à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích bảoo <33',
+    btnNo: 'Khoongggg :3',
+    question:'thế giới bic bảo ng tốt sao len thic bảo<3',
+    btnReply: 'Gửi cho bảo <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: ' bảo biết mà 🥰. Yêu lenn nhiều nhiều 😘😘',
+    messDesc: 'len làm ngyeu bảo nhóooo',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'fffff' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
