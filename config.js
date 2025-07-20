56a1f3705b0a9d52e49c65b38afcf9a52efe890e
baone9 <luonghoaibao220109@gmail.com>
2025-07-20 22:18:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Lenn heoo à!',
+    introDesc: `Thế gian lắm điều bất ngờ
+    như thế mới nhìu sắc màu
+    vì vạy  duyên trời cho ta gặp nhau..`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích bảo <33',
+    btnNo: 'Khoongg :3',
+    question:'Ngoài kia bic bao ng tốt sao lại iu bảo',
+    btnReply: 'Gửi cho bảo <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'bảo biết mà 🥰. Yêu len nhiều nhiều 😘😘',
+    messDesc: 'lenn làm ngyeu bảo nháaa:>',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
